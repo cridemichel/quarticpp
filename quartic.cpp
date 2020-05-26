9d15c7c8a2a574ab25d98860bf389bffd806cb71
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2020-05-26 15:24:20 +0200

    solution of real quartics works

--- quartic.cpp
+++ quartic.cpp
@@ -4,8 +4,9 @@ int main(void)
   quartic<double> Q;
   pvector<double,5> c;
   pvector<complex<double>,4> r;
-  c << 1,1,1,1,1;
+  c << 24,-50,35,-10,1;
   Q.set_coeff(c);
   Q.find_roots(r);
+  r.show("roots");
   return 0;
 }
