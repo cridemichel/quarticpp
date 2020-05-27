8e44ab70f8c296466ffe5c02f96c8f406de5521b
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2020-05-27 15:33:14 +0200

    improved examples

--- quartic_mp.cpp
+++ quartic_mp.cpp
@@ -14,11 +14,17 @@ int main(void)
   pvector<mpreal,5> c;
   pvector<mpcmplx,4> r;
   c << mpreal("16.048044012"),mpreal("-32.072044006"),mpreal("24.036011"),mpreal("-8.006000000000000"),mpreal("1.0"); 
+
   Q.set_coeff(c);
   Q.find_roots(r);
   r.show("roots");
-  cout << setprecision(WP) << "OQS p(" << r[2] << ")=" 
-    << Q.evalpoly(r[2]) << "\n";
-  cout << setprecision(WP) << "MPSOLVE p(4.6356013430199667501755766197408175062413493523496385264695)=" << Q.evalpoly(mpreal("4.6356013430199667501755766197408175062413493523496385264695")) << "\n";
+  int cc=0;
+  for (auto& r0: r)
+    {
+      cout << setprecision(WP) << "root #" << cc <<  "=" << r0 << "\n";
+      cout << setprecision(WP) << "p(#" << cc << ")=" << Q.evalpoly(r0) << "\n\n";
+      cc++;
+    }
+  //cout << setprecision(WP) << "MPSOLVE p(4.6356013430199667501755766197408175062413493523496385264695)=" << Q.evalpoly(mpreal("4.6356013430199667501755766197408175062413493523496385264695")) << "\n";
   return 0;
 }
